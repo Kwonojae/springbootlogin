2c4a5678662f7a7ce307f042bd9c94921e24fcae
Ojea <rnjsdhwo1325@gmail.com>
2022-01-16 15:23:58 +0900

    init

---
+++ src/main/java/com/sesac_login/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.sesac_login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="world") String name, Model model){
+        model.addAttribute("name",name);
+        return "greeting";
+    }
+}
